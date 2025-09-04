50c49a390539ec980cc2d1da1db2cca9dd39cf6f
taureaurouge <henrik.dahlstrom@live.se>
2025-09-04 19:27:37 +0200

    adding more detailed pseudo-code for the generateCalendar-function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 // function genereateCalendar()
-
+// - define an array of all days in the week
+// - define an array of times within each day
+// - loop through days and add to header
+// - for each day: create a slot div for each time
 
 
 // function selectSlot()
