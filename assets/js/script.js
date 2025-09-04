72ce54e8196dbda3847265e8b1093d4e35b9938f
taureaurouge <henrik.dahlstrom@live.se>
2025-09-04 19:56:51 +0200

    adding a generateCalendar-function, styling it do that it's visible aswell as calling it upon DOM-loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,36 @@
+
 // function genereateCalendar()
+
+function generateCalendar() {
+
 // - define an array of all days in the week
 // - define an array of times within each day
+    const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+    const times = ["12:00", "14:00", "16:00", "18:00", "20:00"];
+
+// linking the elements
+    const header = document.querySelector(".calendar-header");
+    const grid = document.querySelector(".calendar-grid");
+
 // - loop through days and add to header
+    days.forEach(day => {
+        const div = document.createElement("div");
+        div.textContent = day;
+        header.appendChild(div);
+    });
+
 // - for each day: create a slot div for each time
+    for (let t of times) {
+        for (let d of days) {
+            const slot = document.createElement("div");
+            slot.classList.add("slot");
+            slot.textContent = t;
+
+            slot.addEventListener("click", () => selectSlot(slot));
+            grid.appendChild(slot);
+        }
+    }
+}
 
 
 // function selectSlot()
@@ -15,3 +43,7 @@
 // function handleSubmit()
 // - prevent default form submission
 // - show confirmation message
+
+document.addEventListener("DOMContentLoaded", () => {
+    generateCalendar();
+});
